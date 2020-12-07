0302a47e7566eaece40eb11b9709d44e23b00a32
Abey Thomas <abeytmas@gmail.com>
2020-12-07 17:03:57 +0530

    create the new PS5 dashboard ui

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,173 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+// import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+    <div className="screen">
+      <header>
+        <section className="media-nav">
+          <a href="#" className="active">
+            Games
           </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="#" >
+            Media
           </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        </section>
+        <section className="settings">
+          <span className="icon">
+            <i className="fa fa-search"/>
+          </span>
+          <span className="icon">
+            <i className="fa fa-cog"/>
+          </span>
+          <span className="icon">
+            <img className="user" src="https://randomuser.me/api/portraits/women/44.jpg"/>
+          </span>
+          <span className="time">
+            8:06 PM
+          </span>
+        </section>
+      </header>
+      <div className="container">
+        <section className="game-nav">
+          <div className="game active">
+            <i className="fa fa-location-arrow"></i>
+          </div>
+          <div className="game">
+            <div className="img" style={{
+              background: "url(https://i.redd.it/8pvdi0pne8a51.png)",
+              backgroundSize: "contain",
+              backgroundPosition: "center",
+              transform: "scale(1.5)"
+            }}>
+            </div>
+          </div>
+          <div className="game">
+            <div className="img" style={{
+              background: "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTEK_UiBvDtgN6dBzTCyUOTOUCQ9QYbWzn0w&usqp=CAU)",
+              backgroundSize: "contain",
+              backgroundPosition: "center",
+              transform: "scale(1.5)"
+            }}>
+            </div>
+          </div>
+          <div className="game">
+            <div className="img" style={{
+              background: "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqRwTEp_F-cFcRumGgScs2DdIBI_7l_3W0zg&usqp=CAU)",
+              backgroundSize: "contain",
+              backgroundPosition: "center",
+              transform: "scale(1.5)"
+            }}>
+            </div>
+          </div>
+          <div className="game">
+            <div className="img" style={{
+              background: "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYS8YGUpMLa2K6Gk2HmjOU_sv9ERUoPRh-Mw&usqp=CAU)",
+              backgroundSize: "contain",
+              backgroundPosition: "center",
+              transform: "scale(1.5)"
+            }}>
+            </div>
+          </div>
+          <div className="game">
+            <div className="img" style={{
+              background: "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHPl5tCVQsxsA87sRRXsOMoS0s1LLGpfi4gw&usqp=CAU)",
+              backgroundSize: "contain",
+              backgroundPosition: "center",
+              transform: "scale(1.5)"
+            }}>
+            </div>
+          </div>
+          <div className="game">
+            <div className="img" style={{
+              background: "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFCwWDH4qB9v9vNpAlXG6rP7jhcNv5I1PyXQ&usqp=CAU)",
+              backgroundSize: "contain",
+              backgroundPosition: "center",
+              transform: "scale(1.5)"
+            }}>
+            </div>
+          </div>
+        </section>
+            <section className="info">
+              <span className="sub-title">
+                Official news from playstation | 11/28/2020
+              </span>
+              <h1>Ready for a PS5 Adventure?</h1>
+              <p>ASTRO's playroom is preloading and waiting for you!
+                Explore the new capabilities of PS5 and feel the world..
+                .
+              </p>
+            </section>
+            
+      </div>
+      <section id="video-trailers">
+              <div className="trailer">
+                <img className="image" 
+                src="https://assets1.ignimgs.com/2020/04/30/491495eaa1d61af478023224602-acv-norwayiconic-male-1588259104701.jpg">
+                </img>
+                <div className="video-info">
+                  <div className="circle-player">
+                    <i className="fa fa-play"></i>
+                  </div>
+                  <span className="title">Assasin's creed </span>
+                </div>
+              </div>
+              <div className="trailer">
+                <img className="image" 
+                src="https://gamingbolt.com/wp-content/uploads/2018/04/Far-Cry-4-bullshot.jpg">
+                </img>
+                <div className="video-info">
+                  <div className="circle-player">
+                    <i className="fa fa-play"></i>
+                  </div>
+                  <span className="title">Far Cry 4 – Ubisoft Montreal </span>
+                </div>
+              </div>
+              <div className="trailer">
+                <img className="image" 
+                src="https://i.pinimg.com/originals/8e/45/ce/8e45ce477f3349ea31fba600dd8cd139.jpg">
+                </img>
+                <div className="video-info">
+                  <div className="circle-player">
+                    <i className="fa fa-play"></i>
+                  </div>
+                  <span className="title">Horizon Zero Dawn  </span>
+                </div>
+              </div>
+              <div className="trailer">
+                <img className="image" 
+                src="https://i2.wp.com/twinfinite.net/wp-content/uploads/2020/03/Demon-Slayer-4.jpg?w=496&h=279&ssl=1">
+                </img>
+                <div className="video-info">
+                  <div className="circle-player">
+                    <i className="fa fa-play"></i>
+                  </div>
+                  <span className="title">Daemon Slayer </span>
+                </div>
+              </div>
+              <div className="trailer">
+                <img className="image" 
+                src="https://i.imgur.com/j3vNOwX.jpg">
+                </img>
+                <div className="video-info">
+                  <div className="circle-player">
+                    <i className="fa fa-play"></i>
+                  </div>
+                  <span className="title">Grand Theft Auto 6 </span>
+                </div>
+              </div>
+              <div className="trailer">
+                <img className="image" 
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS29v41jyUeplA9UYTCttN-m5ZE53JZArh-Ag&usqp=CAU">
+                </img>
+                <div className="video-info">
+                  <div className="circle-player">
+                    <i className="fa fa-play"></i>
+                  </div>
+                  <span className="title">Genshin Impact </span>
+                </div>
+              </div>
+            </section>
     </div>
   )
 }
